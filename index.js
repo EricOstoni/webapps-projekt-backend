8a21395d3aad08d7b684e35c445903243c698ca9
EricOstoni <eostoni@student.unipu.hr>
2023-06-27 19:07:13 +0200

    fix index.js

--- index.js
+++ index.js
@@ -17,13 +17,11 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+
 
 
 let corsOptions = {
-  origin: "https://oilshop-frontend.onrender.com/", 
+  origin: "https://oilshop-frontend.onrender.com", 
   optionsSuccessStatus: 200,
   credentials: true,
 };
